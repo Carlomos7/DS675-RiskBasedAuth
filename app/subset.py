1de539c9cbc3d57b0252eaf7158f97f7ecba1fda
Carlos <cs525@njit.edu>
2024-04-19 11:17:51 -0400

    Refac: stratified sampling

--- app/subset.py
+++ app/subset.py
@@ -1,6 +1,7 @@
 import os
 import kaggle
 import pandas as pd
+import numpy as np
 from pathlib import Path
 from config import get_settings
 
@@ -25,9 +26,36 @@ else:
     print(f"Dataset already downloaded at {data_file_path}")
 
 print("Creating dataframe...")
-
 df = pd.read_csv(data_file_path)
 
+cols = [
+    'IP Address', 'Country', 'Region', 'City', 'ASN', 
+    'User Agent String', 'OS Name and Version', 'Browser Name and Version', 
+    'Device Type', 'Login Successful', 'Is Attack IP', 
+    'Is Account Takeover', 'RTT [ms]', 'Risk Score'
+]
+
+# Filtering to only include necessary columns
+print("Filtering relevant columns...")
+df = df[cols]
+
+# Stratified sampling based on the 'Country' column
+stratify_col = 'Country'
+print(f"Performing stratified sampling based on {stratify_col}...")
+
+# Calculating subset size
+subset_size = int(config.SAMPLE_PERCENTAGE * len(df))
+
+# stratified sampling
+subset_df = df.groupby(stratify_col, group_keys=False).apply(
+    lambda x: x.sample(min(len(x), int(np.rint(subset_size * len(x) / len(df)))))).sample(frac=1).reset_index(drop=True)
+
+# Save the sampled subset to a new CSV file
+subset_filename = sample_data_directory / f"subset_{config.DATA_FILE}"
+subset_df.to_csv(subset_filename, index=False)
+print(f"Subset saved at {subset_filename}.")
+
+"""
 # Calculate 10% of the total number of rows
 print("Calculating percentage of data to sample...")
 subset_size = int(config.SAMPLE_PERCENTAGE * len(df))
@@ -35,7 +63,4 @@ subset_size = int(config.SAMPLE_PERCENTAGE * len(df))
 # Randomly select the subset
 print("Creating subset...")
 subset_df = df.sample(subset_size)
-
-# Save the subset to a new CSV file
-subset_df.to_csv(sample_data_directory / f"subset_{config.DATA_FILE}", index=False)
-print(f"Subset saved at {config.SAMPLE_DATA_DIRECTORY / f'subset_{config.DATA_FILE}'}.")
+"""
