a60ab13e22a851eb920082213e8bd8bad2a1370d
Carlos Segarra <cs525@njit.edu>
2024-04-19 10:39:53 -0400

    Refac: better naming conventions and logic

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -3,28 +3,28 @@ from functools import lru_cache
 from pydantic_settings import BaseSettings
 from pathlib import Path
 
-class Config(BaseSettings):
+class Settings(BaseSettings):
     """Configuration settings for the application"""
 
     APP_ROOT_DIRECTORY: Path = Path.cwd()
     LOG_DIRECTORY: Path = APP_ROOT_DIRECTORY / "logs"
     
     KAGGLE_CONFIG_DIR: Path = Field(..., env="KAGGLE_CONFIG_DIR")
-    DATASET: str = Field(..., env="DATASET")
-    DATA_DIR: Path = Path("data")
-    DATA_FILE: str = Field(..., env="DATA_FILE")
-    SAMPLE_DATA_FILE: str = Field(..., env="SAMPLE_DATA_FILE")
-    SUBSET_DATA_FILE: str = "Subset_" + DATA_FILE
+    DATASET: str = Field(default="dasgroup/rba-dataset", env="DATASET")
+    DATA_DIRECTORY: str = "data"
+    SAMPLE_DATA_DIRECTORY: Path = Path(DATA_DIRECTORY) / "sample_data"
+    DATA_FILE: str = Field(default="rba-dataset.csv", env="DATA_FILE")
+    SAMPLE_PERCENTAGE: float = Field(default=0.1, env="SAMPLE_PERCENT")
 
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
 
 @lru_cache()
-def get_config() -> Config:
-    """Get the configuration settings for the Minecraft Server Manager
+def get_settings() -> Settings:
+    """Get the configuration settings for the application
 
     Returns:
-        Config: The configuration settings
+        Settings: The configuration settings
     """
-    return Config()
+    return Settings()
