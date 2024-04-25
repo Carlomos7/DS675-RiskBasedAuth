cb0bf8fb10fcaa8950e0aad7c690215040d3df40
Carlomos7 <cs525@njit.edu>
2024-04-24 20:49:37 -0400

    Feat: cleaner logistic_regression

--- app/logistic_regression/__init__.py
+++ app/logistic_regression/__init__.py
@@ -7,60 +7,99 @@ from config import get_settings
 from log_config import get_logger
 import matplotlib.pyplot as plt
 
-def log_reg():
-    # Initialize logging and configuration
-    log = get_logger('log_reg')
-    config = get_settings()
+class LogisticRegressionModel:
+    """Logistic regression model class for the RBA model"""
+    
+    def __init__(self):
+        self.log = get_logger(__name__)
+        self.config = get_settings()
+        self.sample_data_directory = Path(self.config.SAMPLE_DATA_DIRECTORY)
+    
+    def load_data(self, filename: str) -> pd.DataFrame:
+        """Load the preprocessed dataset
 
-    # Load the preprocessed dataset
-    log.info("Loading preprocessed dataset...")
-    sample_data_directory = Path(config.SAMPLE_DATA_DIRECTORY)
-    stratified_sample_filename = sample_data_directory / f"pre-processed_subset_{config.DATA_FILE}"
-    df = pd.read_csv(stratified_sample_filename)
+        Args:
+            filename (str): The filename of the preprocessed dataset
 
-    # Splitting dataset into features and target
-    X = df.drop('Is Account Takeover', axis=1)
-    y = df['Is Account Takeover']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+        Returns:
+            pd.DataFrame: The loaded preprocessed dataset
+        """
+        self.log.info("Loading preprocessed dataset...")
+        self.df = pd.read_csv(filename)
+        self.log.info("Dataset loaded successfully.")
+        return self.df
+    
+    def split_data(self, df: pd.DataFrame) -> tuple:
+        """Split the dataset into training and testing sets
+        
+        Args:
+            df (pd.DataFrame): The DataFrame to split
+            
+        Returns:
+            tuple: The training and testing sets
+        """
+        X = df.drop('Is Account Takeover', axis=1)
+        y = df['Is Account Takeover']
+        return train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+    
+    def train_model(self, X_train: pd.DataFrame, y_train: pd.Series) -> LogisticRegression:
+        """
 
-    # Hyperparameter tuning
-    #param_grid = {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000], 'class_weight': ['balanced', None]}
-    #log.info("Hyperparameter tuning...")
-    #grid_search = GridSearchCV(LogisticRegression(max_iter=1000), param_grid, cv=5, scoring='roc_auc')
-    #grid_search.fit(X_train, y_train)
+        Args:
+            X_train (pd.DataFrame): DataFrame of features
+            y_train (pd.Series): Series of target values
 
-    # Training logistic regression model with best parameters
-    log.info("Training logistic regression model with best parameters...")
-    model = LogisticRegression() #max_iter=1000, **grid_search.best_params_
-    model.fit(X_train, y_train)
+        Returns:
+            LogisticRegression: Trained logistic regression model
+        """
+        model = LogisticRegression()
+        model.fit(X_train, y_train)
+        return model
+    
+    def evaluate_model(self, model: LogisticRegression, X_test: pd.DataFrame, y_test: pd.Series) -> tuple:
+        """ Evaluate the model
 
-    # Evaluate the model
-    log.info("Evaluating the model...")
-    y_pred = model.predict(X_test)
-    y_pred_prob = model.predict_proba(X_test)[:, 1]
-    roc_auc = roc_auc_score(y_test, y_pred_prob)
-    conf_matrix = confusion_matrix(y_test, y_pred)
-    class_report = classification_report(y_test, y_pred)
-    log.info(f"ROC-AUC Score: {roc_auc}")
-    log.info(f"Confusion Matrix:\n{conf_matrix}")
-    log.info(f"Classification Report:\n{class_report}")
+        Args:
+            model (LogisticRegression): Model to evaluate
+            X_test (pd.DataFrame): DataFrame of features
+            y_test (pd.Series): Series of target values
 
-    # Plotting the ROC curve
-    log.info("Plotting the ROC curve...")
-    fpr, tpr, _ = roc_curve(y_test, y_pred_prob)
-    roc_auc = auc(fpr, tpr)
-    plt.figure()
-    plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
-    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver Operating Characteristic')
-    plt.legend(loc="lower right")
-    plt.show()
-
-    # Final output
-    log.info("Final model trained, evaluated, and ROC curve plotted successfully.")
-
-log_reg()
+        Returns:
+            tuple: ROC-AUC score, confusion matrix, classification report
+        """
+        y_pred = model.predict(X_test)
+        y_pred_prob = model.predict_proba(X_test)[:, 1]
+        roc_auc = roc_auc_score(y_test, y_pred_prob)
+        conf_matrix = confusion_matrix(y_test, y_pred)
+        class_report = classification_report(y_test, y_pred)
+        return roc_auc, conf_matrix, class_report
+    
+    def plot_roc_curve(self, y_test: pd.Series, y_pred_prob: pd.Series) -> None:
+        fpr, tpr, _ = roc_curve(y_test, y_pred_prob)
+        roc_auc = auc(fpr, tpr)
+        self.log.info("Plotting the ROC curve...")
+        plt.figure()
+        plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+        plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver Operating Characteristic')
+        plt.legend(loc="lower right")
+        plt.show()
+        image_save_path = self.sample_data_directory / 'lr_roc_curve.png'
+        self.log.info("Saving plot ...")
+        plt.savefig(image_save_path)
+        self.log.info(f"ROC curve saved to {image_save_path}")
+        
+    def run(self, filename: str) -> None:
+        df = self.load_data(filename)
+        X_train, X_test, y_train, y_test = self.split_data(df)
+        model = self.train_model(X_train, y_train)
+        roc_auc, conf_matrix, class_report = self.evaluate_model(model, X_test, y_test)
+        self.plot_roc_curve(y_test, model.predict_proba(X_test)[:, 1])
+        self.log.info(f"ROC-AUC Score: {roc_auc}")
+        self.log.info(f"Confusion Matrix:\n{conf_matrix}")
+        self.log.info(f"Classification Report:\n{class_report}")
+        self.log.info("Model trained, evaluated, and ROC curve plotted successfully.")
